1cd1265dd26746e40a85effcc133807ea5aa4cf9
kimmy <shinkew@gmail.com>
2022-08-06 17:05:31 +0900

    commit

--- basic_al/prime_number.py
+++ basic_al/prime_number.py
@@ -50,6 +50,7 @@ n = 26
 
 # 1. 숫자 나열 = 모든 수가 소수(True)인 것으로 초기화 / 0과 1은 제외
 array = [True for i in range(n+1)]
+array[1] = 0 # 1은 소수가 아님
 
 for i in range(2, int(math.sqrt(n)) + 1):
     if array[i]: 
@@ -70,40 +71,3 @@ n = 4
 m = 6
 
 print(n // m)
-
-# 투 포인터
-# => 리스트 내 원소에 음수 데이터가 있으면 활용 X
-
-'''
-* 특정한 합을 가지는 부분 연속 수열 찾기
-* 정렬되어 있는 두 리스트의 합집합 = 병렬 정합(= Merge Sort)
-
-부분 연속 수열 중에서 '특정한 합(M)'을 갖는 수열의 개수 찾기
-1. 시작점start 과 끝점end이 첫 번째 원소의 인덱스(0)을 가리키도록 한다
-2. 현재 부분합이 M과 같다면 카운트 한다.
-3. 현재 부분합이 M보다 작으면 end를 1 증가시킨다.
-4. 현재 부분합이 M보다 크거나 같으면 start를 1 증가시킨다
-5. 모든 경우를 확인할 때까지 2번부터 4번까지 과정을 반복
-'''
-
-n = 5 # 데이터의 개수 = start
-m = 5 # 찾고자 하는 부분합 M
-
-data = [1, 2, 3, 2, 5]
-# start를 오른쪽으로 이동시키면 항상 합이 감소하고, end를 오른쪽으로 이동시키면 항상
-# 합이 증가하기 때문에 투 포인터 알고리즘으로 문제를 해결할 수 있음
-
-cnt = 0
-interval_sum = 0
-end = 0
-
-# start를 차례대로 증가시키며 반복
-for s in range(n): # 0 1 2 3 4
-    while interval_sum < m and end < n:
-        interval_sum += data[end]
-        end += 1
-    print(end)
-    if interval_sum == m:
-        cnt += 1
-    print("합", interval_sum)
-    interval_sum -= data[s]
